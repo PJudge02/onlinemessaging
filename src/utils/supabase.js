72fc857230e0eb1ffb77a4c43a4e6377c3c99f6f
JUDGEPM20 <pjudge2002@gmail.com>
2023-06-03 12:34:38 -0400

    removed file from repository

--- src/utils/supabase.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient('https://deamjoqkudvhztinzmrv.supabase.co',
- 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlYW1qb3FrdWR2aHp0aW56bXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU1MzI4NjIsImV4cCI6MjAwMTEwODg2Mn0.w_9XhdnJOjbpFVsYmcFVaOt03svaAeDv_nJfRXJR_no')
