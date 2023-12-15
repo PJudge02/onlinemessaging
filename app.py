23a2d36853d8e4fc199cd6179bde76d1d3cd7564
GVMartin23 <MartinGV19@gcc.edu>
2023-12-15 11:39:44 -0500

    backend encrypt

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 import src.backend.Encryption as enc
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, DateTime, Text, BigInteger
 from datetime import datetime
 from flask_cors import CORS
+
 # psycopg2
 
 app = Flask(__name__)
@@ -32,10 +34,10 @@ class User(db.Model):
 class Message(db.Model):
     __tablename__ = "messages"
     messageid = Column(BigInteger, primary_key=True)
-    useridfrom = Column(BigInteger, nullable=False)
-    useridto = Column(BigInteger, nullable=False)
+    userfrom = Column(BigInteger, nullable=False)
+    userto = Column(BigInteger, nullable=False)
     message = Column(Text, nullable=False)
-    encrypted_key = Column(Text)
+    key = Column(Text)
     nonce = Column(Text)
     signature = Column(Text)
     created_at = Column(DateTime, nullable=False)
@@ -46,33 +48,53 @@ def newUser():
     key = enc.rsa_gen_keypair()
     info = request.json
     now = datetime.utcnow().isoformat()
-    user = User.query.filter_by(email=info['email']).all()
-    if user: 
+    user = User.query.filter_by(email=info["email"]).all()
+    if user:
         return "", 200
     user = User(
         created_at=now,
-        name=info["name"], # type: ignore
-        email=info["email"], #type: ignore
-        private_key=key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.PKCS8,
-            encryption_algorithm=serialization.NoEncryption(),
-        ),
-        public_key=key.public_key().public_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo,
-        ),
-    ) # type: ignore
+        name=info["name"],  # type: ignore
+        email=info["email"],  # type: ignore
+        private_key=enc.rsa_serialize_private_key(key),
+        public_key=enc.rsa_serialize_public_key(key.public_key()),
+    )  # type: ignore
     db.session.add(user)
     db.session.commit()
     return "", 200
 
 
-@app.post("/api/flask/message/<string:sender>/<string:receiver>/encrypt/")
-def encrypt_message(sender, receiver):
+@app.post("/api/flask/message/encrypt/")
+def encrypt_message():
+    info = request.json
+    ## get sender
+    sender = User.query.filter_by(email=info["sender"]).first_or_404()
+    ## get receiver
+    receiver = User.query.filter_by(email=info["receiver"]).first_or_404()
+
+    message: str = info["message"]
+    enc_message: bytes = message.encode()
+
+    sender_private: rsa.RSAPrivateKey = enc.rsa_deserialize_private_key(sender.private_key.encode())  # type: ignore
+    receiver_public: rsa.RSAPublicKey = enc.rsa_deserialize_public_key(
+        receiver.public_key.encode()
+    ) #type: ignore
+
+    key, nonce, encrypted_message, signature = enc.encrypt_message_with_aes_and_rsa(
+        receiver_public, enc_message, sender_private
+    )
+
+    message = Message(
+        userfrom=sender.email,
+        userto=receiver.email,
+        message=encrypted_message,
+        key=key,
+        nonce=nonce,
+        signature=signature,
+        created_at=datetime.utcnow().isoformat(),
+    ) # type: ignore
     return "", 200
 
 
-@app.post("/api/flask/message/<string:sender>/<string:receiver>/decrypt/")
+@app.post("/api/flask/message/decrypt/")
 def decrypt_message(sender, receiver):
     return "", 200
